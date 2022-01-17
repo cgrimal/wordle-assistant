b4c29b7581e5c299956ce5ab36a843f208d7734e
cgrimal <clement.grimal@sigfox.com>
2022-01-17 17:23:17 +0100

    README + flake8 + formatting

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# wordle-assistant
+# Wordle Assistant
+
+Tools to remove all the fun from playing Wordle
+
+## Installation
+
+You should create a virtual environment first with your preferred tool.
+
+Then install the project with [poetry](https://python-poetry.org/):
+
+```bash
+poetry install
+```
+
+## Usage
+
+```bash
+python src/stats.py --dictionary-path data/dictionary_fr.txt --word-length 8
+```
+
+## Contributing
+
+### Dependencies
+
+Adding dependencies to the project can be done with a simple:
+
+```bash
+poetry add <package-name>
+```
+
+### Formatting
+
+Always run `black` and `isort` on all the code of the package (including the example project):
+
+```bash
+black src
+isort src
+```
+
+### Additional linter
+
+Even though `black` does a great job a formatting the code, some lint warnings can sneak through it, so
+we also use `flake8` (with a custom configuration):
+
+```bash
+flake8 src
+```
+
+### Type hinting
+
+To check the type hinting is correct, simply run:
+
+```bash
+mypy --pretty src
+```
--- src/stats.py
+++ src/stats.py
@@ -17,7 +17,7 @@ def filter_word_length(dictionary: List[str], word_length: int) -> List[str]:
 
 
 def compute_global_frequencies(dictionary: List[str]) -> Dict[str, float]:
-    frequencies = dict.fromkeys(list("ABCDEFGHIJKLMNOPQRSTUVXYZ"))
+    frequencies = dict.fromkeys(list("ABCDEFGHIJKLMNOPQRSTUVXYZ"), 0.0)
     all_words = "".join(dictionary)
     total_letters = len(all_words)
     for letter in frequencies:
@@ -26,11 +26,15 @@ def compute_global_frequencies(dictionary: List[str]) -> Dict[str, float]:
 
 
 def compute_word_appearance_frequencies(dictionary: List[str]) -> Dict[str, float]:
-    return compute_global_frequencies(list(map(lambda word: "".join(set(word)), dictionary)))
+    return compute_global_frequencies(
+        list(map(lambda word: "".join(set(word)), dictionary))
+    )
 
 
 def pretty_print_frequencies(frequencies: Dict[str, float]) -> None:
-    for letter, frequency in sorted(frequencies.items(), key=lambda item: item[1], reverse=True):
+    for letter, frequency in sorted(
+        frequencies.items(), key=lambda item: item[1], reverse=True
+    ):
         click.secho(f"{letter}: {round(frequency * 100, 2)}%")
 
 
@@ -41,12 +45,16 @@ def main(dictionary_path: str, word_length: int) -> None:
     dictionary = read_dictionary(dictionary_path)
     click.secho(f"Total words in dictionary: {len(dictionary)}", bold=True, fg="green")
     filtered_dictionary = filter_word_length(dictionary, word_length)
-    click.secho(f"Words of {word_length} letters in dictionary: {len(filtered_dictionary)}", bold=True, fg="green")
-    click.secho(f"Letter global frequencies:", bold=True, fg="green")
+    click.secho(
+        f"Words of {word_length} letters in dictionary: {len(filtered_dictionary)}",
+        bold=True,
+        fg="green",
+    )
+    click.secho("Letter global frequencies:", bold=True, fg="green")
     pretty_print_frequencies(compute_global_frequencies(filtered_dictionary))
-    click.secho(f"Letter word appearance frequencies:", bold=True, fg="green")
+    click.secho("Letter word appearance frequencies:", bold=True, fg="green")
     pretty_print_frequencies(compute_word_appearance_frequencies(filtered_dictionary))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
